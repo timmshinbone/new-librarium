85e3c4bc22feb0f7295ac2f423a86cefe889e063
Timm Schoenborn <timmschoenborn@gmail.com>
2021-09-06 19:57:31 -0400

    converts app to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { Component, Fragment } from 'react';
 import './index.scss';
 import 'semantic-ui-css/semantic.min.css'
 import NavBar from './components/shared/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <h1>Librarium</h1>
-    </div>
-  );
+class App extends Component {
+  render () {  
+    return (
+      <Fragment className="App">
+        <NavBar />
+        <h1>Librarium Body Goes Here</h1>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
