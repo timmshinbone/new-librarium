5888dc69ff771511da2d85d767da3a90c4ccb376
Timm Schoenborn <timmschoenborn@gmail.com>
2021-09-06 19:32:10 -0400

    Header update before name change to NavBar

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -3,16 +3,26 @@ import { Container, Menu, Button } from 'semantic-ui-react'
 
 const Header = () => {
     // WHY AREN'T MY HOOKS WORKING? DO I WANT/NEED THEM IN THIS CASE??
-    const { activeItem, setActiveItem } = useState('home')
+    const [activeItem, setActiveItem] = useState('home')
 
-    const handleItemClick = (e) => {setActiveItem(e.target.name)}
+    const handleItemClick = (e, { name }) => {
+        console.log(e.target) 
+        setActiveItem(name)
+    }
+    console.log('this is activeItem in header', activeItem);
+    
     return (
 			<Container>
 				<h1>Librarium</h1>
 				<Menu>
 					<Menu.Item
+						name='home'
+						active={activeItem === 'home'}
+						onClick={handleItemClick}
+					/>
+					<Menu.Item
 						name='my books'
-						active={activeItem === 'all books'}
+						active={activeItem === 'my books'}
 						onClick={handleItemClick}
 					/>
 					<Menu.Item
@@ -20,10 +30,10 @@ const Header = () => {
 						active={activeItem === 'all books'}
 						onClick={handleItemClick}
 					/>
-                    <Menu.Item position='right'>
-                        <Button>Log In</Button>
-                        <Button>Sign Up</Button>
-                    </Menu.Item>
+					<Menu.Item position='right'>
+						<Button>Log In</Button>
+						<Button>Sign Up</Button>
+					</Menu.Item>
 				</Menu>
 			</Container>
 		)
