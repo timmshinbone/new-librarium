6c92229b7dc2a64256e0ffb6ac09cbf553d72b8d
Timm Schoenborn <timmschoenborn@gmail.com>
2021-09-08 11:39:59 -0400

    updates NavBar to change based on authentication

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -11,8 +11,54 @@ const NavBar = ({ user, msgAlert }) => {
         setActiveItem(name)
 		// code for calling a msgAlert, use in all auth components
     }
-
     console.log('this is activeItem in header', activeItem);
+
+	const unAuthOptions = (
+		<Menu>
+			<Menu.Item
+				name='home'
+				active={activeItem === 'home'}
+				onClick={handleItemClick}
+			/>
+			<Menu.Item position='right'>
+				<Button basic as='a' href='#sign-in'>
+					Log In
+				</Button>
+				<Button basic as='a' href='#sign-up'>
+					Sign Up
+				</Button>
+			</Menu.Item>
+		</Menu>
+	)
+
+	const authOptions = (
+		<Menu>
+			<Menu.Item
+				name='home'
+				active={activeItem === 'home'}
+				onClick={handleItemClick}
+			/>
+			<Menu.Item
+				name='my books'
+				active={activeItem === 'my books'}
+				onClick={handleItemClick}
+			/>
+			<Menu.Item
+				name='all books'
+				active={activeItem === 'all books'}
+				onClick={handleItemClick}
+			/>
+			<Menu.Item position='right'>
+				<Button basic as='a' href='#change-pw'>
+					Change Password
+				</Button>
+				<Button basic color='red' as='a' href='#sign-out'>
+					Sign Out
+				</Button>
+			</Menu.Item>
+		</Menu>
+	)
+
     
     return (
 			<Container style={{ paddingTop: '1rem' }} textAlign='center'>
@@ -20,31 +66,7 @@ const NavBar = ({ user, msgAlert }) => {
 				<Header as='h1' icon>
 					Librarium
 				</Header>
-				<Menu>
-					<Menu.Item
-						name='home'
-						active={activeItem === 'home'}
-						onClick={handleItemClick}
-					/>
-					<Menu.Item
-						name='my books'
-						active={activeItem === 'my books'}
-						onClick={handleItemClick}
-					/>
-					<Menu.Item
-						name='all books'
-						active={activeItem === 'all books'}
-						onClick={handleItemClick}
-					/>
-					<Menu.Item position='right'>
-						<Button basic as='a' href='#sign-in'>
-							Log In
-						</Button>
-						<Button basic as='a' href='#sign-up'>
-							Sign Up
-						</Button>
-					</Menu.Item>
-				</Menu>
+				{ user ? authOptions : unAuthOptions }
 				<Divider />
 			</Container>
 		)
